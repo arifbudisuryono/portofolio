a3e6278b08995eaca6e0998712fe6e751cff5ac7
Arif Budi Suryono <arifmaldini22@gmail.com>
2026-05-26 21:05:31 +0700

    fix: link url

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        wedding: resolve(__dirname, 'wedding.html')
+      }
+    }
+  }
 })
